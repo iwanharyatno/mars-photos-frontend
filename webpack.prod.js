6a955abaf1904caab880b8a933552b66b2a52f0c
iwanharyatno <iwnharry61@gmail.com>
2022-09-27 03:36:33 +0700

    Add CleanWebpackPlugin to clean production static assets on build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,11 @@
 const common = require('./webpack.common.js');
+
 const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 });
